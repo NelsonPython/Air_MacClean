f009bc5fc0a6bf0bb18b610fcfda5effb45e516b
NelsonPython <34105635+NelsonPython@users.noreply.github.com>
2019-08-13 17:51:13 -0700

    Update textStatus.py

--- code/textStatus.py
+++ code/textStatus.py
@@ -4,7 +4,7 @@ def textStatus(msg):
                   you need your own API KEY
         '''
         try:
-                r = requests.post(url="https://maker.ifttt.com/trigger/IP/with/key/bA5CPmVFeQJTcYzAXWU8dP", data={"value1": msg})
+                r = requests.post(url="https://maker.ifttt.com/trigger/IP/with/key/YOUR-KEY=HERE", data={"value1": msg})
                 print("Text status ", r.status_code, r.reason)
         except:
                 print("Text status ", r.status_code, r.reason)
