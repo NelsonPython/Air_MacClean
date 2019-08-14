ccf9d70b2fd33e6597188621e916441026a8dc53
NelsonPython <34105635+NelsonPython@users.noreply.github.com>
2019-08-13 17:50:32 -0700

    Update getIP.py

--- code/getIP.py
+++ code/getIP.py
@@ -9,8 +9,6 @@ def getIP():
         '''
         s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
         try:
-                #s.connect(('172.20.10.9',1))   #iphone hotspot
-                s.connect(('192.168.86.29',1))  #LA lab
                 s.connect(('192.168.1.9',1))    #PS lab
 
                 IP = s.getsockname()[0]
